4135d0b6bb3be8fcd91f1f96aa7a46519fade9c5
ryne-shelton <85508647+ryne-shelton@users.noreply.github.com>
2022-01-24 19:46:23 -0800

    Add files via upload

---
+++ timer.c
@@ -0,0 +1,15 @@
+/*
+ * timer.c - Source code for for timer program
+ *
+ * @authors 
+ *          Ibrahim Binmahfood (ibrah5@pdx.edu)
+ *          Ryne Shelton       (ryn2@pdx.edu)
+ *
+ * @date 24-Jan-2022
+ *
+ * (ECE 362 Winter 2022 HW #2) 
+ *
+ * Description:  
+ *
+ *
+ */
